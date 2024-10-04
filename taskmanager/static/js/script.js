54555c7bad55043b57ed1ae840c22aa817b36265
Atsuko Coffey <atsucoffey@gmail.com>
2024-10-04 18:16:01 +0000

    Add the task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,11 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     //  sidenav initialization
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmmm yyyy",
+      i18n: {done: "select"}
+    })
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
 
-  // Or with jQuery
-
-//   $(document).ready(function(){
-//     $('.sidenav').sidenav();
-//   });
-       
